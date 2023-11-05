535d5473d0976de6c06a7043c66e41596315d288
HelmiKaroui <mouhamedhelmi.karoui@esprit.tn>
2023-11-05 02:04:24 +0100

    test2

--- src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -1,10 +1,10 @@
 package tn.esprit.devops_project.services;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.StockRepository;
 
@@ -12,11 +12,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-class StockServiceImplTest {
+@ExtendWith(MockitoExtension.class)
+public class StockServiceImplTest {
 
     @InjectMocks
     private StockServiceImpl stockService;
@@ -24,65 +25,82 @@ class StockServiceImplTest {
     @Mock
     private StockRepository stockRepository;
 
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
-    void addStock() {
+    void testAddStock() {
+        // Créez un stock de test
         Stock stockToSave = new Stock();
         stockToSave.setIdStock(1L);
         stockToSave.setTitle("Example Stock");
 
+        // Configurez le comportement de stockRepository.save() pour retourner le stock de test
         when(stockRepository.save(stockToSave)).thenReturn(stockToSave);
 
+        // Appelez la méthode de service à tester
         Stock result = stockService.addStock(stockToSave);
 
+        // Vérifiez que le résultat est le même que le stock de test
         assertEquals(stockToSave, result);
+
+        // Vérifiez que la méthode stockRepository.save() a été appelée une fois
+        verify(stockRepository, times(1)).save(stockToSave);
     }
 
     @Test
-    void retrieveStock() {
+    void testRetrieveStock() {
         Long stockId = 1L;
+
+        // Créez un stock de test
         Stock stock = new Stock();
         stock.setIdStock(stockId);
         stock.setTitle("Example Stock");
 
+        // Configurez le comportement de stockRepository.findById() pour retourner le stock de test
         when(stockRepository.findById(stockId)).thenReturn(Optional.of(stock));
 
+        // Appelez la méthode de service à tester
         Stock result = stockService.retrieveStock(stockId);
 
+        // Vérifiez que le résultat est le même que le stock de test
         assertEquals(stock, result);
+
+        // Vérifiez que la méthode stockRepository.findById() a été appelée une fois avec l'ID correct
+        verify(stockRepository, times(1)).findById(stockId);
     }
 
     @Test
-    void retrieveStock_NotFound() {
+    void testRetrieveStock_NotFound() {
         Long stockId = 1L;
 
+        // Configurez le comportement de stockRepository.findById() pour retourner Optional.empty()
         when(stockRepository.findById(stockId)).thenReturn(Optional.empty());
 
-        // Utilisez assertThrows pour vérifier qu'une exception est lancée lorsque le stock n'est pas trouvé
-        assertThrows(NullPointerException.class, () -> stockService.retrieveStock(stockId));
+        // Appelez la méthode de service à tester et vérifiez qu'une exception est lancée
+        assertThrows(NullPointerException.class, () -> {
+            stockService.retrieveStock(stockId);
+        });
+
+        // Vérifiez que la méthode stockRepository.findById() a été appelée une fois avec l'ID correct
+        verify(stockRepository, times(1)).findById(stockId);
     }
 
     @Test
-    void retrieveAllStock() {
+    void testRetrieveAllStock() {
+        // Créez une liste de stocks de test
         List<Stock> stockList = new ArrayList<>();
-        Stock stock1 = new Stock();
-        stock1.setIdStock(1L);
-        stock1.setTitle("Stock 1");
-        stockList.add(stock1);
+        stockList.add(new Stock(1L, "Stock 1", null));
+        stockList.add(new Stock(2L, "Stock 2", null));
+        stockList.add(new Stock(3L, "Stock 3", null));
 
-        Stock stock2 = new Stock();
-        stock2.setIdStock(2L);
-        stock2.setTitle("Stock 2");
-        stockList.add(stock2);
-
+        // Configurez le comportement de stockRepository.findAll() pour retourner la liste de stocks de test
         when(stockRepository.findAll()).thenReturn(stockList);
 
+        // Appelez la méthode de service à tester
         List<Stock> result = stockService.retrieveAllStock();
 
+        // Vérifiez que le résultat contient la même liste de stocks de test
         assertEquals(stockList, result);
+
+        // Vérifiez que la méthode stockRepository.findAll() a été appelée une fois
+        verify(stockRepository, times(1)).findAll();
     }
 }
